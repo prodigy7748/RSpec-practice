8943aaa3049bec72058aa42cd9688f20c286a517
Phil Lee <prodigy7748g@gmail.com>
2021-02-03 00:09:43 +0800

    add methods

--- coffee_spec.rb
+++ coffee_spec.rb
@@ -1,12 +1,30 @@
 RSpec.describe 'A cup of coffee' do
+
+  class Coffee
+    def price
+      30
+    end
+
+    def ingredients
+      @ingredients ||= []
+    end
+
+    def add(ingredient)
+      ingredients << ingredient
+    end
+  end
+
   it 'costs NTD30' do
     coffee = Coffee.new
     expect(coffee.price).to eq(30)
   end
-end
 
-class Coffee
-  def price
-    30
+  context 'with milk' do
+    coffee = Coffee.new
+
+    it 'costs NTD45' do
+      coffee.add('milk')
+      expect(coffee.price).to eq(45)
+    end
   end
 end
